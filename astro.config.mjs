9cac1a75d4cd6987ad737d6f4ec025eac4a5e90f
mreeve29 <michael.s.reeve@gmail.com>
2023-07-31 08:35:14 -0400

    update site in astro config, update github actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,6 @@ import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://ishgup.github.io/ishgup-site",
+    site: "https://ishgup.github.io/",
     integrations: [react(), tailwind(), sitemap(), mdx()],
 });
